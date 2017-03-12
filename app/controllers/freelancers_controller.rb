b9cd10606ec65b917d915e1e5415a6a0e4800332
Lucas Angelon <lucas.a.arouca@gmail.com>
2017-03-12 11:16:08 +0800

    feat(freelancers): add controller and index action

---
+++ app/controllers/freelancers_controller.rb
@@ -0,0 +1,11 @@
+class FreelancersController < ApplicationController
+
+  # GET /
+  #
+  # Displays the list of freelancers in the database.
+  def index
+    #@freelancers = Freelancer.all
+  end
+end
+
+
