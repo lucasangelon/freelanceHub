06a1c2e0271e2cc1f8b0b0e681517b45bc7fa071
Lucas Angelon <lucas.a.arouca@gmail.com>
2017-03-12 12:00:22 +0800

    feat(freelancers): add new and create actions and routes

--- app/controllers/freelancers_controller.rb
+++ app/controllers/freelancers_controller.rb
@@ -6,6 +6,31 @@ class FreelancersController < ApplicationController
   def index
     #@freelancers = Freelancer.all
   end
+
+  # GET /new
+  #
+  # Generates a new Freelancer object for the view.
+  def new
+    @freelancer = Freelancer.new
+  end
+
+  # POST /
+  #
+  # Accepts the parameters for a new freelancer and executes the insert.
+  def create
+    @freelancer = Freelancer.new(freelancer_params)
+
+    if @freelancer.save
+      redirect_to index
+    else
+      render :new
+    end
+  end
+
+  private
+    def freelancer_params
+      params.require(:freelancer).permit(:first_name, :last_name, :specialty)
+    end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   root to: 'freelancers#index'
-
+  get 'new', to: 'freelancers#new'
+  post '/', to: 'freelancers#create'
 end
